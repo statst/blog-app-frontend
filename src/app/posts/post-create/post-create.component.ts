1158428c1f855f3702f3d3aa94f31d8cfa95e718
jyoti <jshindems@gmail.com>
2020-07-16 16:06:15 -0400

    added edit and delete buttons

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,26 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Post } from '../post.model';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
-
+import { PostsService } from "../posts.service";
 
 @Component({
-  selector: 'app-post-create',
-  templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
 })
-
-
-export class PostCreateComponent{
+export class PostCreateComponent {
   enteredTitle = "";
   enteredContent = "";
-  @Output() postCreated = new EventEmitter<Post>();
-  onAddPost(form: NgForm){
-    if(form.invalid){
+
+  constructor(public postsService: PostsService) { }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
       return;
     }
-    const post: Post = {title: form.value.title, content: form.value.content }
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
-
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { Subscription } from 'rxjs'
 import { Post } from '../post.model'
 import { PostsService } from '../posts.service'
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy{
   // posts = [
   //   { title: 'First Post', content: "This is the first post\'s content" },
   //   { title: 'Second Post', content: "This is the second post\'s content" },
@@ -14,9 +16,20 @@ export class PostListComponent {
   //   { title: 'Fourth Post', content: "This is the fourth post\'s content" },
   //   { title: 'Fifth Post', content: "This is the fifth post\'s content" },
   // ];
-  @Input() posts = [];
-postsService: PostsService;
-  constructor(postsService: PostsService) {
-    this.postsService = postsService;
+  posts: Post[] = [];
+  private postsSub: Subscription
+
+  constructor( public postsService: PostsService) {
+
+  }
+  ngOnInit(){
+    this.posts=this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) =>{
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
   }
 }
